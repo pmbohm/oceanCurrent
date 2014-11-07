1f7f45f0ba2b3d9d4114b3cc24bc9fe161b9c1fa
littlebrothergeorge <pmbohm@utas.edu.au>
2014-11-07 17:08:49 +1100

    working Bootstrap version

---
+++ footer.php
@@ -0,0 +1,32 @@
+</div>
+
+<div class="jumbotronFooter">
+    <div class="container">
+        <footer class="row">
+            <div class="col-md-4">
+                <p>If you've found this information useful, see something wrong, or have a suggestion, please let us
+                    know.
+                    All feedback is very welcome. For help and information about this site
+                    please contact <a href="mailto:info@emii.org.au">info@emii.org.au</a>, noting that our systems are
+                    not monitored 24/7 (being research infrastructure rather than operational).</p>
+            </div>
+
+            <div class="col-md-8">
+                <p>Use of this web site and information available from it is subject to our
+                    <a href="http://imos.org.au/imostermsofuse0.html">Conditions of use</a>
+                </p>
+                <p>&copy; 2014 IMOS</p>
+            </div>
+        </footer>
+    </div>
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+<script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: pmbohm
+ * Date: 7/11/14
+ * Time: 3:41 PM
+ */ 
