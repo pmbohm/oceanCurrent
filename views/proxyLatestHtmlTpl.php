72840c8c9202ade94263c84554e952a95c86a847
littlebrothergeorge <pmbohm@utas.edu.au>
2014-11-25 21:52:10 +1100

    fix for <-previous date picker next-> button breakage in Dark theme commit

--- views/proxyLatestHtmlTpl.php
+++ views/proxyLatestHtmlTpl.php
@@ -10,48 +10,44 @@
 <?php } ?>
 
 <nav class="navbar navbar-default mapNavbar" role="navigation">
-    <div class="container">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
-                    data-target="#bs-example-navbar-collapse-1">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-        </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <nav class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="pager mapPager">
-                <li class="previous"><a class="" href="<?php echo $folderName ?>/<?php echo $previous ?>"
-                                        onclick="setProxiedHtms('latest','<?php echo $folderName ?>/<?php echo $previous ?>');return false;"
-                                        alt="Previous available time period for <?php echo $folderName ?>"
-                                        title="Previous available time period for <?php echo $folderName ?>">
-                        <span aria-hidden="true">&larr;</span> Previous</a></li>
-                <li><a href="<?php echo $datePickerUrl ?>" alt="date picker" onclick="setProxiedHtms('index','<?php echo$datePickerUrl?>');return false;">Date Picker</a></li>
-                <li class="next"><a class="" href="<?php echo $folderName ?>/<?php echo $next ?>"
-                                    onclick="setProxiedHtms('latest','<?php echo $folderName ?>/<?php echo $next ?>');return false;"
-                                    alt="Next available time period for <?php echo $folderName ?>"
-                                    title="Next available time period for <?php echo $folderName ?>">Next
-                        <span aria-hidden="true">&rarr;</span></a></li>
-            </ul>
-        </nav>
-        <?php //.navbar-collapse ?>
-    </div><?php //.container-fluid ?>
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <nav class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="pager mapPager">
+            <li class="previous"><a class="" href="<?php echo $folderName ?>/<?php echo $previous ?>"
+                                    onclick="setProxiedHtms('latest','<?php echo $folderName ?>/<?php echo $previous ?>');return false;"
+                                    alt="Previous available time period for <?php echo $folderName ?>"
+                                    title="Previous available time period for <?php echo $folderName ?>">
+                    <span aria-hidden="true">&larr;</span> Previous</a></li>
+            <li><a href="<?php echo $datePickerUrl ?>" alt="date picker"
+                   onclick="setProxiedHtms('index','<?php echo $datePickerUrl ?>');return false;">Date Picker</a></li>
+            <li class="next"><a class="" href="<?php echo $folderName ?>/<?php echo $next ?>"
+                                onclick="setProxiedHtms('latest','<?php echo $folderName ?>/<?php echo $next ?>');return false;"
+                                alt="Next available time period for <?php echo $folderName ?>"
+                                title="Next available time period for <?php echo $folderName ?>">Next
+                    <span aria-hidden="true">&rarr;</span></a></li>
+        </ul>
+    </nav>
+    <?php //.navbar-collapse ?>
 </nav>
-
 <ul class="nav navbar-right nav-pills mini-nav">
     <li role="presentation" class="dropdown">
-    <a href="#"
-       data-toggle="modal"
-       data-target="#basicModal"
-       role="button"
-       onClick="setModelImageUrl('<?php echo $imageUrl ?>')" >
-        <span class="glyphicon glyphicon-resize-full" aria-hidden="true"></span> see full size </a></li>
+        <a href="#"
+           data-toggle="modal"
+           data-target="#basicModal"
+           role="button"
+           onClick="setModelImageUrl('<?php echo $imageUrl ?>')">
+            <span class="glyphicon glyphicon-resize-full" aria-hidden="true"></span> see full size </a></li>
 </ul>
-
-
 <div class="mapImage">
     <a href="#"
        data-toggle="modal"
