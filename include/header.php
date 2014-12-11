ed4424524a603694db3ae7a22b07e486108cc23d
littlebrothergeorge <pmbohm@utas.edu.au>
2014-12-12 10:31:15 +1100

    New layout of toplinks as per Madelines design

--- include/header.php
+++ include/header.php
@@ -42,8 +42,8 @@ $BASEDIR = getBaseDir(getcwd(), constant('CONTEXT')); // todo does this work in 
                 </a>
                 <h3 class="highlightedHeader">&#8220; Up to date ocean information around Australia. &#8221;</h3>
             </header>
-            <p><a class="btn btn-primary" href="<?php echo $BASEDIR ?>news.php" role="button">Latest News &raquo;</a>
-            </p>
+            <!--p><a class="btn btn-primary" href="<?php echo $BASEDIR ?>news.php" role="button">Latest News &raquo;</a>
+            </p-->
         </div>
         <nav class="navbar navbar-default navbar-ocheader" role="navigation">
             <div class="container">
@@ -56,31 +56,56 @@ $BASEDIR = getBaseDir(getcwd(), constant('CONTEXT')); // todo does this work in 
                     </button>
                 </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="mainNavbar" >
+                <div class="collapse navbar-collapse" id="mainNavbar">
                     <ul class="nav navbar-nav ">
                         <li><a class="navbar-brand" href="<?php echo $BASEDIR ?>index.php" alt="home page">Home</a>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Products<span
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Data<span
                                     class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="<?php echo $BASEDIR ?>sst.php" alt="Sea Surface Temperature">Sea
                                         Surface Temperature</a></li>
                                 <li><a href="<?php echo $BASEDIR ?>oceancolour.php" alt="Ocean Colour">Ocean Colour</a>
                                 </li>
+                                <li class="divider"></li>
+                                <li><a href="<?php echo $BASEDIR ?>argo.php" alt="Argo">Argo</a>
+                                </li>
+                                <li><a href="<?php echo $BASEDIR ?>currmeters.php" alt="Current Meters">Current
+                                        Meters</a>
+                                </li>
+                                <li><a href="<?php echo $BASEDIR ?>gliders.php" alt="Gliders">Gliders</a>
+                                </li>
+                                <li><a href="<?php echo $BASEDIR ?>seals.php" alt="Seals">Seals</a>
+                                </li>
                             </ul>
                         </li>
-                        <li><a class="" href="<?php echo $BASEDIR ?>news.php" alt="news items">News</a>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">News<span
+                                    class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a class="" href="<?php echo $BASEDIR ?>latestnews.php"
+                                       alt="Latest News Items">Latest</a>
+                                </li>
+                                <li><a class="" href="<?php echo $BASEDIR ?>technicalnews.php"
+                                       alt="Technical News Items">Technical</a>
+                                </li>
+                            </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Information<span
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Source Data<span
                                     class="caret"></span></a>
                             <ul class="dropdown-menu" role="menu">
                                 <li><a href="<?php echo $BASEDIR ?>whatsshown.php" alt="Whats shown">What's
                                         Shown</a></li>
                                 <li><a href="<?php echo $BASEDIR ?>profiles/whatsshown.php" alt="Whats shown">What's
                                         Shown - Profiles</a></li>
-                                <li class="divider"></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Descriptions<span
+                                    class="caret"></span></a>
+                            <ul class="dropdown-menu" role="menu">
                                 <li><a href="<?php echo $BASEDIR ?>acknowl.php"
                                        alt="Acknowledgements">Acknowledgements</a></li>
                                 <li><a href="<?php echo $BASEDIR ?>glossary.php" alt="Glossary">Glossary</a></li>
@@ -90,16 +115,12 @@ $BASEDIR = getBaseDir(getcwd(), constant('CONTEXT')); // todo does this work in 
                             </ul>
                         </li>
                     </ul>
-
-
                     <ul class="nav navbar-nav navbar-right">
                         <li><a class="navbar-active" href="http://imos.org.au" alt="IMOS home page">IMOS</a></li>
                         <li><a class="navbar-active" href="http://portal.imos.org.au" alt="IMOS ocean portal">
                                 IMOS Ocean Portal</a></li>
                     </ul>
                 </div><?php //.navbar-collapse ?>
-
-
             </div><?php //.container ?>
         </nav>
     </div>
